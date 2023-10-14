9185e68c7c61196a336c038e0924b52aafad1aaa
simpKumari <137141557+simpKumari@users.noreply.github.com>
2023-10-14 06:22:50 +0000

    datatype

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello")  //we are using node.js not browser
+console.log("simpi")
+
+let name ="simpi" //string
+let age = 15  //number
+let isLoggedIn = false //boolean
+let city; //undefined
+
+
+
+// object
+console.log(typeof 15)
