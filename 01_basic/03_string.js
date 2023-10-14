68cc64252b2a63fabaf21181be8919abf60b7271
simpKumari <137141557+simpKumari@users.noreply.github.com>
2023-10-14 11:40:28 +0000

    string

---
+++ 01_basic/03_string.js
@@ -0,0 +1,15 @@
+const name ="simpi"
+const repoCount =50
+
+// console.log(name + repoCount+ "value");
+
+console.log(`hello my name is ${name} and repo count is ${repoCount}`);
+
+let gamename = new String('simpi kumari')
+console.log(gamename[0]);
+
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf('m'));
