751402204add18f569e7839da62c94e8d7f730b1
simpKumari <137141557+simpKumari@users.noreply.github.com>
2023-10-14 10:49:06 +0000

    converting,comparision

--- 01_basic/01_datatype.js
+++ 01_basic/01_datatype.js
@@ -3,12 +3,66 @@
 // alert("hello")  //we are using node.js not browser
 console.log("simpi")
 
-let name ="simpi" //string
+let someNum = 22   //string
+let someName = String(someNum)
+console.log(typeof someName);
+
+
+
 let age = 15  //number
-let isLoggedIn = false //boolean
+
 let city; //undefined
 
 
 
 // object
-console.log(typeof 15)
+console.log(typeof "15")
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let isLoggedIn = 1  //boolean
+let booleanValue = Boolean(isLoggedIn)
+console.log(booleanValue)
+
+// "" =false------boolean
+// "simpi"= true-------boolean
+
+
+//-------------------operationn-------------
+let value = 3
+let negValue= -value
+console.log(negValue)
+
+console.log(2+2);
+
+let str= "hello"
+let str2= "world"
+let str3= str+str2
+console.log(str3);
+
+// console.log("2" +3);
+// console.log("2" +2+2);
+// console.log(1 +3 +"3");
+
+console.log(+true);
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCover= 22
+++gameCover;
+console.log(gameCover);
+
+// ====---------comparision------------
+
+console.log("2" > 1); //string>number
+
+console.log(null>0);
+console.log(null ==0);
+console.log(null >=0);
+
+console.log("2" === 2);// ===datatype ko check krta hai
+
