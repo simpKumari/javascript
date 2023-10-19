38465223723eab930bcbd470d78cc53081895d49
simpKumari <137141557+simpKumari@users.noreply.github.com>
2023-10-19 07:09:03 +0000

    object

--- 01_basic/object.js
+++ 01_basic/object.js
@@ -9,4 +9,38 @@ const jsuser= {
 }
 
 console.log(jsuser.email);
-console.log(jsuser["email"]);
+Object.freeze(jsuser)
+jsuser.email ="ggss@gmail.com"
+console.log(jsuser);
+
+
+
+const tinderUser ={}
+
+tinderUser.id ="gsadg12"
+tinderUser.name ="samm"
+tinderUser.isLoggedIn= false
+
+console.log(tinderUser);
+
+const regularUser ={
+    name:"simpi",
+    fullName:{
+        userName:{
+            fname:"simpi",
+            lname: "mehta"
+        }
+    }
+}
+console.log(regularUser.fullName.userName.lname);
+
+
+const obj1 ={1:"a",2:"b"}
+
+const obj2 ={3:"a",4:"b"}
+
+// const obj3 =Object.assign({},obj1,obj2)  //{}traget , objt1or obj2 is source
+// console.log(obj3);
+
+const obj3 ={...obj1, ...obj2}
+console.log(obj3);
