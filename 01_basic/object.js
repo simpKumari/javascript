373f3974238bd5dd86a8a46ea57e68d57e73821b
simpKumari <137141557+simpKumari@users.noreply.github.com>
2023-10-19 06:43:18 +0000

    object

---
+++ 01_basic/object.js
@@ -0,0 +1,12 @@
+console.log("hello")
+
+const jsuser= {
+    name: "simpi",
+    age: 16,
+    email: "jhdh@gmail.com",
+    isLoggedIn: false,
+    lastLoginDay: ["monday", "tuesday"]
+}
+
+console.log(jsuser.email);
+console.log(jsuser["email"]);
